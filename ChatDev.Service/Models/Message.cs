c2eb5e1942068b5d5f4b647a06fd697b034f6af0
Bence <gyorfi.bence@stud.uni-obuda.hu>
2022-10-04 09:01:31 +0200

    Message class added

---
+++ ChatDev.Service/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatDev.Service.Models
+{
+    public class Message
+    {
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+        public string SenderName { get; set; }
+    }
+}

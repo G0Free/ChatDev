0d5a09e7851323658bdf957032d83c0b3ae92f9d
Bence <gyorfi.bence@stud.uni-obuda.hu>
2022-10-04 09:08:42 +0200

    Controller added

---
+++ ChatDev.Service/Controllers/MessageController.cs
@@ -0,0 +1,21 @@
+﻿using ChatDev.Service.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatDev.Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageController : Controller
+    {
+        private List<Message> messages;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

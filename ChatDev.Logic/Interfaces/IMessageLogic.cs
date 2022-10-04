4588be0e751eeff02fce61bbd8570cd7880b4bc7
Bence <gyorfi.bence@stud.uni-obuda.hu>
2022-10-04 09:26:22 +0200

    Minor changes

---
+++ ChatDev.Logic/Interfaces/IMessageLogic.cs
@@ -0,0 +1,14 @@
+﻿using ChatDev.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatDev.Logic.Interfaces
+{
+    interface IMessageLogic
+    {
+        void Create(Message item);
+    }
+}
